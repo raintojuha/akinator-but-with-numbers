33a8201c5acb9b6fa08feea00f07588d8fbba35d
Juha Rainto <juha.rainto@symbio.com>
2023-04-21 13:33:56 +0300

    Main mechanic finished. User input removed

--- main.py
+++ main.py
@@ -1,65 +1,134 @@
+from random import randint
+from time import sleep
+
 class Guess:
 
-    def __init__(self, target):
+    def __init__(self):
         self.currentGuess = 0
         self.minPossible = 0
-        self.maxPossible = 0
-        self.target = target
+        self.maxPossible = 1000
 
-class Message:
+        self.banterList = [
+            "banter 1",
+            "banter 2",
+            "banter 3",
+            "banter 4",
+            "banter 5"
+        ]
 
-    def __init__(self):
-        self.tooLarge = "Too large to handle!"
-        self.negative = "Stay positive. Your number I mean!"
-        self.zero = "Okay. I guess that's a number."
-        self.notAnuber = "Do you know what a number is"
+        self.curseList = [
+            "curse 1",
+            "curse 2",
+            "curse 3",
+            "curse 4",
+            "curse 5"
+        ]
+
+        self.celebrateList = [
+            "celebrate 1",
+            "celebrate 2",
+            "celebrate 3",
+            "celebrate 4",
+            "celebrate 5"
+        ]
+    
+
+    def banter(self):
+        print(self.banterList[randint(0, 4)])
+        sleep(randint(1, 3))
+
+    def curse(self):
+        print(self.curseList[randint(0, 4)])
+        sleep(randint(1, 3))
+
+    def celebrate(self):
+        print(self.celebrateList[randint(0, 4)])
+        sleep(randint(1, 3))
+
+    def tooHigh(self):
+        if self.currentGuess <= self.maxPossible:
+            self.maxPossible = self.currentGuess -1
+
+    def tooLow(self):
+        if self.currentGuess >= self.minPossible:
+            self.minPossible = self.currentGuess +1
 
 
+    def makeAGuess(self):
+        self.banter()
 
-
-def takeUserInput(question):
-    msg = Message()
-    raw = input(f"{question}: ")
-    goodInput = False
-    while goodInput == False:
-        isInt = True
-        try:
-            int(raw)
-        except ValueError:
-            print(msg.notAnuber)
-            isInt = False
-
-        if isInt:
-            if int(raw) < 0:
-                print(msg.negative)
-                pass
-            elif int(raw) > 1000:
-                print(msg.tooLarge)
-                pass
-            else:
-                goodInput = True
-                if raw == 0:
-                    print(msg.zero)
-                return raw
-        
-        raw = input("> ")
-            
+        if self.minPossible == self.maxPossible:
+            return 1
+        else:
+            self.currentGuess = randint(self.minPossible, self.maxPossible)
+            print(f"I'm thinking of {self.currentGuess}")
+            return 0
 
 
 
 def main():
 
-    userTarget = takeUserInput("Number please")
+    game = Guess()
 
-    game = Guess(userTarget)
-    print(game.currentGuess)
-    print(game.minPossible)
-    print(game.maxPossible)
-    print(game.target)
+    print("Welcome \nI'll be your guesser for the game \nThink of a number between 0 and 1000\n\n")
 
     
+    play = False
+    while not play:
+        rawInput = input("Type 'play' when ready!")
+        if rawInput == "play":
+            play = True
+        else:
+            rawInput = print("Incorrect.")
 
+
+    
+    print("Let's begin")
+    sleep(2)
+
+
+    correct = False
+
+    while not correct:
+
+        if game.makeAGuess() == 1: # INSTA WIN
+            print(f"Your number HAS to be {game.currentGuess}.")
+            pass
+        else:
+            case = checkGuess()
+            if case == "H":
+                game.tooLow()
+            elif case == "L":
+                game.tooHigh()
+            elif case == "C":
+                game.celebrate()
+                correct = True
+
+        print(game.minPossible)
+        print(game.maxPossible)
+
+    print("Thank you for playing.")
     return
+
+
+
+def checkGuess():
+    while True:
+        rawInput = input("Is your number, (H)igher, (L)ower, (C)orrect?")
+
+        if rawInput == "H": #HIGHER
+            return "H"
+        elif rawInput == "L": #LOWER
+            return "L"
+        elif rawInput == "C": #CORRECT
+            return "C"
+        else:
+            print("Incorrect.")
+            rawInput = input("> ")
+
+
+
+
 
 
 if __name__ == '__main__':
